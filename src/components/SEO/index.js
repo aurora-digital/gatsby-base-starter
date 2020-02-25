adfa0022da27a4b01141b8d991dcf4ef9f65a6fc
Luís Cracel <luis.cracel@gmail.com>
2020-02-25 17:43:35 +0000

    Fix some errors regarding SEO and Layout components

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,26 +1,55 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        keywords
+      }
+    }
+  }
+`;
+
 function SEO({ title, description, keywords }) {
+  const data = useStaticQuery(query);
+  const {
+    title: siteTitle,
+    description: siteDescription,
+    keywords: siteKeywords,
+  } = data.site.siteMetadata;
+
   return (
     <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
+      <title>{title || siteTitle}</title>
+      <meta name="description" content={description || siteDescription} />
+      <meta name="keywords" content={keywords || siteKeywords} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta property="og:title" content={title || siteTitle} />
+      <meta
+        property="og:description"
+        content={description || siteDescription}
+      />
       <meta property="og:image" content="logo.png" />
     </Helmet>
   );
 }
 
 SEO.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  keywords: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  keywords: PropTypes.string,
+};
+
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  keywords: null,
 };
 
 export default SEO;
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
   <div>
     <SEO title="Home Page" />
     <Header />
+
     <Layout>
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
