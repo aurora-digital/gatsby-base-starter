b0a5e7c49d963177d58c1fa5c9906c481ebc371e
Francisco Sousa <josesousa9000@gmail.com>
2020-05-07 18:29:01 +0100

    HOTFIX: Remove incorrect usage of `header` and replace with `nav`

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,12 @@ import styles from "./index.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.root}>
+    <nav className={styles.root}>
       <div className={styles.inner}>
         <Link to="/" className={styles.link}>
           Gatsby
         </Link>
       </div>
-    </header>
+    </nav>
   );
 }
